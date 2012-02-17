96f2d614aa45c0beb6b4953431f72446056354ad
Adam Davis <adavis71@kent.edu>
2012-02-17 01:56:15 -0500

    first commit

---
+++ src/wotw/app/WotwMain.java
@@ -0,0 +1,25 @@
+package wotw.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class WotwMain extends Activity {
+
+	public WotwMain() {
+		// TODO Auto-generated constructor stub
+	}
+	
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+
+        }
+    
+
+}
